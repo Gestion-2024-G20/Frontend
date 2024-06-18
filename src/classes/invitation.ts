18f2396a73b12bf8ba2c8e0c96a667682105e53f
Agustina Schmidt <aschmidt@fi.uba.ar>
2024-06-17 21:54:14 -0300

    cambio id grupo por nombre en invitaciones y requests

--- src/classes/invitation.ts
+++ src/classes/invitation.ts
@@ -1,6 +1,7 @@
 export class Invitation {
     id_invitation: number = 0;
     id_group: number = 0;
+    group_name: string = "";
     id_user: number = 0;
     status: string = "";
     is_request: boolean = false;
